0e246cd5b70bad7e75da12bf8a32b65be07eea87
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-27 11:14:17 -0600

    working auth jwt

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+
+    render json: {user_id: @user.id}
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+
+    header = request.headers["Authorization"]
+    token = header.split(" ").last
+
+    payload = JsonWebToken.decode(token)
+
+    if (@user.id == payload["user_id"].to_i)
+      @user.update(user_params)
+      render json: { user_id: @user.id}
+    else
+      render json: { error: "Sorry Dude"}, status: :unauthorized
+    end
+  end
+
+
+  private
+  def user_params
+      params.permit(:username, :password)
+  end
+end
