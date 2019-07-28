b79b7d990a59ccf0ab9926c77882e494dd0aeb53
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-28 12:45:20 -0600

    scaffolded Job model - made has_many relation w/user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_154916) do
+ActiveRecord::Schema.define(version: 2019_07_28_184236) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
